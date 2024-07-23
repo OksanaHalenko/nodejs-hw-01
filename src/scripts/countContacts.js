723d2059d980ac280c2e0955d298209105fd75f0
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-07-23 13:13:53 +0200

    add remove all contacts and count contacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,15 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    let count = contacts.length;
+    return count;
+  } catch (err) {
+    console.error('Error reading contacts:', err);
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([]), 'utf-8');
+    console.log('All your contacts have been deleted');
+  } catch (err) {
+    console.error('Error deleting all contacts:', err);
+  }
+};
 
 removeAllContacts();
