fbe290ac866ab2e82c6a936d533b701d2651052b
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-07-23 13:21:03 +0200

    add removing last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let contacts = JSON.parse(data);
+
+    if (contacts.length > 0) {
+      contacts.pop();
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+      console.log('The last contact have been deleted');
+    } else {
+      console.log('There is nothing to delete');
+    }
+  } catch (err) {
+    console.error('Error deleting the last contact:', err);
+  }
+};
 
 removeLastContact();
