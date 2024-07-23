50bede993e1cf5bf5bd7bd7b3b7b3055246b20d2
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-07-23 12:59:10 +0200

    add creating one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,18 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let contacts = JSON.parse(data);
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    console.log('One contact generated and added to your collection');
+  } catch (err) {
+    console.error('Error generating contact:', err);
+  }
+};
 
 addOneContact();
